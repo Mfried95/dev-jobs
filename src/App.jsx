8ab4b48a2a486c301e9dd910292b81751af03ffa
Mfried95 <m.friedman1995@gmail.com>
2023-07-18 17:44:21 -0400

    Created and styled header, also responsive for tablet and desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
+import Header from './components/Header'
+
 
 function App() {
  
 
   return (
     <>
-     <div>
-      <h2>Hello World!</h2>
-     </div>
+     <Header/>
     </>
   )
 }

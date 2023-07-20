1037234113a81fef70c396bc7b41100b77cbaf0d
Mfried95 <m.friedman1995@gmail.com>
2023-07-20 13:39:16 -0400

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Home from './pages/Home'
+import JobDetails from './pages/JobDetails'
+import { Routes, Route } from "react-router-dom"
+
 
 function App() {
  
 
   return (
-    <>
-    <Home/>
-    </>
+    <div className="App">
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/job/:id" element={<JobDetails/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/pages/JobDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import data from '../utils/data'
+
+const JobDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JobDetails

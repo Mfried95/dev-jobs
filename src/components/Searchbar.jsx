ec00a20242e81d97ea30bf11f52f6f30936dce2a
Mfried95 <m.friedman1995@gmail.com>
2023-08-03 14:37:25 -0400

    Refactored code in searchBar componenet

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -6,32 +6,21 @@ import searchBlue from "../assets/desktop/icon-search-blue.svg";
 import data from "../data/data.js";
 
 const Searchbar = () => {
-  const [searchParams, setSearchParams] = useState({
-    search: "",
-    fullTime: false,
-    location: "",
-  });
+  const [searchParams, setSearchParams] = useState("");
+  const [fullTime, setFullTime] = useState(false);
+  const [location, setLocation] = useState("");
   const [filteredData, setFilteredData] = useState([]);
 
-  const handleInputChange = (event) => {
-    setSearchParams({
-      ...searchParams,
-      search: event.target.value,
-    });
+  const handleSearchChange = (event) => {
+    setSearchParams(event.target.value);
   };
 
   const handleCheckboxChange = (event) => {
-    setSearchParams({
-      ...searchParams,
-      fullTime: event.target.checked,
-    });
+    setFullTime(event.target.checked);
   };
 
   const handleLocationChange = (event) => {
-    setSearchParams({
-      ...searchParams,
-      location: event.target.value,
-    });
+    setLocation(event.target.value);
   };
 
   const handleSearch = (event) => {
@@ -39,12 +28,10 @@ const Searchbar = () => {
 
     // Filter the data based on the search parameters
     const filteredData = data.filter((item) => {
-      const { search, fullTime, location } = searchParams;
-
       // Check if the item's position contains the search keyword
       if (
-        search &&
-        !item.position.toLowerCase().includes(search.toLowerCase())
+        searchParams &&
+        !item.position.toLowerCase().includes(searchParams.toLowerCase())
       ) {
         return false;
       }
@@ -62,65 +49,60 @@ const Searchbar = () => {
         return false;
       }
 
+      setSearchParams("");
+      setFullTime(false);
+      setLocation("");
+
       return true;
     });
 
     console.log(filteredData);
 
     setFilteredData(filteredData);
-
-    setSearchParams({
-      search: "",
-      fullTime: false,
-      location: "",
-    });
   };
 
   return (
     <div className="searchbar-container">
       <form onSubmit={handleSearch} className="search-form-container">
-      { /** Mobile-searchbar bar stays hidden if its not in mobile view */ }
+        {/* Mobile-searchbar stays hidden if it's not in mobile view */}
         <div className="mobile-searchbar">
           <input
             className="title-filter-input"
             type="text"
             placeholder="Filter by title..."
-            value={searchParams.search}
-            onChange={handleInputChange}
+            value={searchParams}
+            onChange={handleSearchChange}
           />
-          <button type="submit" onSubmit={handleSearch}>
+          <button type="submit">
             <img src={search} alt="" />
           </button>
         </div>
         <div className="desktop-searchbar">
-          <button type="submit" onSubmit={handleSearch}>
-            <img src={searchBlue} alt="" />
+          <button type="submit">
+            <img src={searchBlue} alt="search" />
           </button>
           <input
             className="title-filter-input"
             type="text"
             placeholder="Filter by title..."
-            value={searchParams.search}
-            onChange={handleInputChange}
+            value={searchParams}
+            onChange={handleSearchChange}
           />
-
           <input
             className="location-form"
             type="text"
             placeholder="Filter by location"
-            value={searchParams.location}
+            value={location}
             onChange={handleLocationChange}
           />
-
           <input
             className="checkbox-form"
             type="checkbox"
             id="fullTime"
-            checked={searchParams.fullTime}
+            checked={fullTime}
             onChange={handleCheckboxChange}
           />
           <label htmlFor="fullTime">Full-Time</label>
-
           <button type="submit" className="desktop-tablet-button">
             Search
           </button>
